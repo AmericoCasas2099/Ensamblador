edde1f318f72cecdd3d44271f759c56b78beca8a
AmericoCasas2099 <l22140750@queretaro.tecnm.mx>
2024-10-16 09:13:24 -0600

    Inicio

---
+++ Sintaxis.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ensamblador
+{
+    
+    public class Sintaxis : Lexico
+    {
+       
+        public Sintaxis()
+        {
+            nextToken();
+        }
+        public Sintaxis(string nombre) : base(nombre)
+        {
+            nextToken();
+        }
+        public void match(string espera)
+        {
+
+            if (Contenido == espera)
+            {
+                
+                nextToken();
+            }
+            else
+            {
+                //Console.Write("Linea "+lineaError+" ");
+                throw new Error("Sintaxis: se espera un ("+espera+") en la linea "+linea,log);
+            }
+        }
+        public void match(Tipos espera)
+        {
+            if (Clasificacion == espera)
+            {
+                nextToken();
+            }
+            else
+            {
+                throw new Error("Sintaxis: se espera un "+espera + " en la línea "+ linea,log);
+            }
+        }
+    }
+}
